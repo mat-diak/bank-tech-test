7d85d7eb186fada36bc6931eba3199b3667a913e
mikitaro-27 <matt.diak@gmail.com>
2022-02-21 14:19:54 +0000

    Create getBalance method for Account

---
+++ main/account.js
@@ -0,0 +1,5 @@
+export class Account {
+  getBalance() {
+    return 0
+  }
+}
--- tests/account.test.js
+++ tests/account.test.js
@@ -1,5 +1,12 @@
-describe('', () => {
-  it('', () => {
-    
+import { Account } from '../main/account.js'
+
+describe('Account', () => {
+  describe('getBalance', () => {
+    describe('when initialised', () => {
+      it('returns 0', () => {
+        const account = new Account();
+        expect(account.getBalance()).toEqual(0)
+      })
+    })
   })
 });
